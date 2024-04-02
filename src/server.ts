0d1e78eda4a06858b98e856bb49f162548f57a4b
ecthon <ecthon@gmail.com>
2024-04-02 16:48:59 -0400

    feature: create schema validation for body data with fastify-type-provider-zod

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,34 @@
 import fastify from "fastify";
+import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 import z from "zod";
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 import { generateSlug } from "./utils/generate-slug";
 
 const app = fastify()
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 const prisma = new PrismaClient({
   log: ['query'],
 })
 
+app.withTypeProvider<ZodTypeProvider>().post('/events', {
+  schema: {
+    body: z.object({
+      title: z.string().min(4),
+      details: z.string().nullable(),
+      maximumAttendees: z.number().int().positive().nullable(),
+    }),
+    response: {
+      201: z.object({
+        eventId: z.string().uuid(),
+      })
+    },
+  }
+}, async (request, reply) => {
 
-app.post('/events', async (request, reply) => {
-  const createEventSchema = z.object({
-    title: z.string().min(4),
-    details: z.string().nullable(),
-    maximumAttendees: z.number().int().positive().nullable(),
-  })
-
-  const { title, details, maximumAttendees } = createEventSchema.parse(request.body)
+  const { title, details, maximumAttendees } = request.body
 
   const slug = generateSlug(title)
 
