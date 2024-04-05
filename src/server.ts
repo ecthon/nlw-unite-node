cd4727f2611c18eac71b2c8edde1b0d330828218
ecthon <ecthon@gmail.com>
2024-04-05 00:39:38 -0400

    feature: create route get-attendee-badge

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod
 import { createEvent } from "./routes/create-events";
 import { registerForEvent } from "./routes/register-for-events";
 import { getEvent } from "./routes/get-events";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
 
 const app = fastify()
 
@@ -12,6 +13,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createEvent)
 app.register(registerForEvent)
 app.register(getEvent)
+app.register(getAttendeeBadge)
 
 app.listen({ port: 3333}).then(() => {
   console.log("HTTP server running")
