620e13c40a94dc5c4bd8c382171c3637ffcf7088
ecthon <ecthon@gmail.com>
2024-04-05 00:24:18 -0400

    feature: create route get-events

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-events";
 import { registerForEvent } from "./routes/register-for-events";
+import { getEvent } from "./routes/get-events";
 
 const app = fastify()
 
@@ -10,6 +11,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent)
 app.register(registerForEvent)
+app.register(getEvent)
 
 app.listen({ port: 3333}).then(() => {
   console.log("HTTP server running")
