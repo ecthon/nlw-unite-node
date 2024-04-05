7978d8aedb160dc0c4321cd359dc77790f7814cf
ecthon <ecthon@gmail.com>
2024-04-04 21:24:50 -0400

    feature: bed-request

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-events";
+import { registerForEvent } from "./routes/register-for-events";
 
 const app = fastify()
 
@@ -8,6 +9,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent)
+app.register(registerForEvent)
 
 app.listen({ port: 3333}).then(() => {
   console.log("HTTP server running")
