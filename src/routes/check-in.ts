4aa79afb687d6f37161b6b22db95058166880778
ecthon <ecthon@gmail.com>
2024-04-08 19:04:42 -0400

    feature: generating documentation with swagger; error handling - error handler

--- src/routes/check-in.ts
+++ src/routes/check-in.ts
@@ -1,16 +1,19 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { z } from "zod";
+import z from "zod";
 import { prisma } from "../lib/prisma";
+import { BadRequest } from "./_errors/bad-request";
 
 export async function checkIn(app: FastifyInstance) {
   app
     .withTypeProvider<ZodTypeProvider>()
     .get('/attendees/:attendeeId/check-in', {
       schema: {
+        summary: 'Check-in an attendee',
+        tags: ['check-ins'],
         params: z.object({
-          attendeeId: z.coerce.number().int(),
-        }), 
+          attendeeId: z.coerce.number().int()
+        }),
         response: {
           201: z.null(),
         }
@@ -25,7 +28,7 @@ export async function checkIn(app: FastifyInstance) {
       })
 
       if (attendeeCheckIn !== null) {
-        throw new Error('Attendee already checked in!')
+        throw new BadRequest('Attendee already checked in!')
       }
 
       await prisma.checkIn.create({
